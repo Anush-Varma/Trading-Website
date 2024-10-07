53754f87e19dcb8b2922f74757013e4ffb3a5203
Anush Varma <anush.formal@outlook.com>
2024-10-08 00:27:36 +0100

    Planning and added proxy for server and axios setup

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -1,5 +1,12 @@
 import Nav from "./components/Nav";
 
+// TO DO: HJome page should kinda be siilar to Vijay page
+// Make it more visually pleasing.
+
+// Home page should have Login, Search bar, Tutorial,
+// Favourites / WatchList, Ability to view and click each
+// gylph to enlarge
+
 function HomePage() {
   return (
     <div>
--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const corsOptions = {
-    origin: "http://localhost:5173",
+const express = require("express");
+const app = express();
+const port = 8080;
 
-}
+const cors = require("cors");
+app.use(cors());
+
+// TO DO: Do a get request to get API for stocks and data and
+// use polygon.io for data
+// use data to display to frontend
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
 
 // This is where you define routes for the application GET, POST ...
 
-app.use(cors(corsOptions))
-
-
-
-app.listen(8080, () => {
-    console.log("Server has started on port 8080")
-})
+app.listen(port, () => {
+  console.log("Server has started on port 8080");
+});
