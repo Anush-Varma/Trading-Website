fed4423d491107ecffc737dd01d1e55db492131e
Anush Varma <anush.formal@outlook.com>
2024-10-10 18:14:02 +0100

    button styling and renamed index.js to server.js

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,12 +6,16 @@ function Nav() {
     <div className="nav-bar">
       <div className="left">
         <Button text="StockSage"></Button>
-        <Button text="Tutorial"></Button>
         <Button text="Watch List"></Button>
       </div>
+      <div className="search-bar">
+        <span>Search Bar</span>
+      </div>
+
       <div className="right">
-        <Button text="Login"></Button>
+        <Button text="Tutorial"></Button>
         <Button text="Sign Up"></Button>
+        <Button text="Profile"></Button>
       </div>
       {/* Have card like components where the cards can expand and 
       shrink depending on if gylph is clicked and expand with larger graphs
