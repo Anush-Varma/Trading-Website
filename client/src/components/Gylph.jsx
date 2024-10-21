adb8317a1ffc07a7e5eadf8f711280ca20f367a2
Anush Varma <anush.formal@outlook.com>
2024-10-21 23:15:04 +0100

    button poitioning maths

--- client/src/components/Gylph.jsx
+++ client/src/components/Gylph.jsx
@@ -3,17 +3,17 @@ import React from "react";
 import "../styles/gylph.css";
 
 function Gylph() {
-  //   const radius = 150;
-  //   const buttons = ["A", "B", "C"];
-  //   const center = 200; // change
+  const radius = 150;
+  const buttons = ["A", "B", "C"];
+  const center = 200; // change
 
-  //   const getButtonPosition = (index, totalButtons) => {
-  //     const angle = (index / totalButtons) * (2 * Math.PI);
-  //     const x = center + radius * Math.cos(angle);
-  //     const y = center + radius * Math.sin(angle);
+  const getButtonPosition = (index, totalButtons) => {
+    const angle = (index / totalButtons) * (2 * Math.PI);
+    const x = center + radius * Math.cos(angle);
+    const y = center + radius * Math.sin(angle);
 
-  //     return { x, y };
-  //   };
+    return { x, y };
+  };
 
   return (
     <div className="circle-container">
