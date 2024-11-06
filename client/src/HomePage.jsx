5ed648bc490a368fbbae452b8046b3b13597bd47
Anush Varma <anush.formal@outlook.com>
2024-11-06 20:18:33 +0000

    formatting

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -1,12 +1,6 @@
 import Nav from "./components/Nav";
 import Card from "./components/Card";
 import "./styles/homePage.css";
-// TO DO: HJome page should kinda be siilar to Vijay page
-// Make it more visually pleasing.
-
-// Home page should have Login, Search bar, Tutorial,
-// Favourites / WatchList, Ability to view and click each
-// gylph to enlarge
 
 function HomePage() {
   const cardCount = Array.from({ length: 5 });
